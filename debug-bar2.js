77bbe4280d60d89ade77fd0882071f1fd8ea910d
tuongroth <44430600+tuongroth@users.noreply.github.com>
2023-11-13 19:56:07 +0200

    Update debug-bar2.js

--- debug-bar2.js
+++ debug-bar2.js
@@ -16,29 +16,27 @@ var swiper = new Swiper("#home", {
   });
 
 
-  var swiper = new Swiper(".mySwiper", {
-	slidesPerView: 1,
-	spaceBetween: 10,
-	pagination: {
-	  el: ".swiper-pagination",
-	  clickable: true,
-	},
-	breakpoints: {
-	  "@0.00": {
-		slidesPerView: 1,
-		spaceBetween: 10,
-	  },
-	  "@0.75": {
-		slidesPerView: 2,
-		spaceBetween: 20,
-	  },
-	  "@1.00": {
-		slidesPerView: 3,
-		spaceBetween: 40,
-	  },
-	  "@1.50": {
-		slidesPerView: 4,
-		spaceBetween: 50,
-	  },
-	},
-  });
+   const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      on: {
+        autoplayTimeLeft(s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress);
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+      }
+    });
